b1fdac67e0c80dcada39a6d701973868581aa15b
AoiHosizora <a970335605@hotmail.com>
2020-02-08 01:32:10 +0800

    feat(*): init proj

---
+++ main.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name         Github events
+// @namespace    https://github.com/Aoi-hosizora
+// @version      1.0
+// @description  let github shows users and repos activity events
+// @author       Aoi-hosizora
+// @match        https://github.com/*
+// @copyright    2020+, Aoi-hosizora
+// @require      https://code.jquery.com/jquery-1.12.4.js
+// @require      https://code.jquery.com/ui/1.12.1/jquery-ui.js
+// @grant        GM_setValue
+// @grant        GM_getValue
+// ==/UserScript==
+
+document.addEventListener('DOMContentLoaded', () => {
+    onLoaded();
+});
+
+function onLoaded() {
+
+}
---
+++ readme.md
@@ -0,0 +1,4 @@
+# GithubEvents_TamperMonkey
+
++ Let github shows users and repos activity events
++ TemperMonkey version of [Aoi-hosizora/GithubEventsExt](https://github.com/Aoi-hosizora/GithubEventsExt)
