dbdf1fffadc0139525e1512ac56d76044b24acc4
AoiHosizora <a970335605@hotmail.com>
2020-02-08 01:54:47 +0800

    feat(*): *

--- main.js
+++ main.js
@@ -1,15 +1,19 @@
 // ==UserScript==
 // @name         Github events
-// @namespace    https://github.com/Aoi-hosizora
+// @namespace    https://github.com/Aoi-hosizora/GithubEvents_TamperMonkey
 // @version      1.0
 // @description  let github shows users and repos activity events
 // @author       Aoi-hosizora
-// @match        https://github.com/*
+// @match        http*://github.com/*
 // @copyright    2020+, Aoi-hosizora
-// @require      https://code.jquery.com/jquery-1.12.4.js
-// @require      https://code.jquery.com/ui/1.12.1/jquery-ui.js
+
 // @grant        GM_setValue
 // @grant        GM_getValue
+// @grant        GM_addStyle
+// @run-at       document-start
+// @require      http://code.jquery.com/jquery-3.4.1.min.js
+// @require      http://code.jquery.com/ui/1.12.1/jquery-ui.min.js
+// @resource     html https://raw.githubusercontent.com/Aoi-hosizora/GithubEventsExt/master/public/options.html
 // ==/UserScript==
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -17,5 +21,43 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 function onLoaded() {
+    console.log('Start');
+    adjustGithubUi();
+    mainInject();
+}
 
+function adjustGithubUi() {
+    const ghShadowHeads = $('.gh-header-shadow');
+    if (ghShadowHeads && ghShadowHeads.length > 0) {
+        ghShadowHeads[0].style.zIndex = '89';
+    }
+
+    const ghYourGistTag = $('.dropdown-menu.dropdown-menu-sw .dropdown-item[data-ga-click$="your gists"]');
+    const ghUsernameTag = $('.dropdown-menu.dropdown-menu-sw .dropdown-item[data-ga-click$="Signed in as"] strong');
+    const username = ghUsernameTag.text();
+    $('<a>', {
+        role: 'menuitem',
+        class: 'dropdown-item',
+        href: `/${username}?tab=followers`,
+        text: 'Your followers',
+        'data-ga-click': 'Header, go to followers, text:your followers'
+    }).insertBefore(ghYourGistTag);
+    $('<a>', {
+        role: 'menuitem',
+        class: 'dropdown-item',
+        href: `/${username}?tab=followings`,
+        text: 'Your followings',
+        'data-ga-click': 'Header, go to followings, text:your followings'
+    }).insertBefore(ghYourGistTag);
+    $('<a>', {
+        role: 'menuitem',
+        class: 'dropdown-item',
+        href: '/',
+        text: 'Github Homepage',
+        'data-ga-click': 'Header, go to homepage, text:homepage'
+    }).insertAfter(ghYourGistTag);
+}
+
+function mainInject() {
+    // !!!
 }
--- readme.md
+++ readme.md
@@ -2,3 +2,8 @@
 
 + Let github shows users and repos activity events
 + TemperMonkey version of [Aoi-hosizora/GithubEventsExt](https://github.com/Aoi-hosizora/GithubEventsExt)
+
+### References
+
++ [Tampermonkey中文文档（部分）](https://blog.csdn.net/abc45628/article/details/53919135)
++ [GM_addStyle equivalent in TamperMonkey](https://stackoverflow.com/questions/23683439/gm-addstyle-equivalent-in-tampermonkey)
