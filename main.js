8504b4aaf34bf864c5eda5282a4cb77bcac10cbd
AoiHosizora <a970335605@hotmail.com>
2020-02-08 15:11:25 +0800

    feat(*): temp commit for .user.js

--- main.js
+++ main.js
@@ -1,25 +1,3 @@
-// ==UserScript==
-// @name         Github events
-// @namespace    https://github.com/Aoi-hosizora/GithubEvents_TamperMonkey
-// @version      1.0
-// @description  let github shows users and repos activity events
-// @author       Aoi-hosizora
-// @match        http*://github.com/*
-// @copyright    2020+, Aoi-hosizora
-
-// @grant        GM_setValue
-// @grant        GM_getValue
-// @grant        GM_addStyle
-// @run-at       document-start
-// @require      http://code.jquery.com/jquery-3.4.1.min.js
-// @require      http://code.jquery.com/ui/1.12.1/jquery-ui.min.js
-// @resource     html https://raw.githubusercontent.com/Aoi-hosizora/GithubEventsExt/master/public/options.html
-// ==/UserScript==
-
-document.addEventListener('DOMContentLoaded', () => {
-    onLoaded();
-});
-
 function onLoaded() {
     console.log('Start');
     adjustGithubUi();
@@ -27,29 +5,29 @@ function onLoaded() {
 }
 
 function adjustGithubUi() {
-    const ghShadowHeads = $('.gh-header-shadow');
+    const ghShadowHeads = _$('.gh-header-shadow');
     if (ghShadowHeads && ghShadowHeads.length > 0) {
         ghShadowHeads[0].style.zIndex = '89';
     }
 
-    const ghYourGistTag = $('.dropdown-menu.dropdown-menu-sw .dropdown-item[data-ga-click$="your gists"]');
-    const ghUsernameTag = $('.dropdown-menu.dropdown-menu-sw .dropdown-item[data-ga-click$="Signed in as"] strong');
+    const ghYourGistTag = _$('.dropdown-menu.dropdown-menu-sw .dropdown-item[data-ga-click$="your gists"]');
+    const ghUsernameTag = _$('.dropdown-menu.dropdown-menu-sw .dropdown-item[data-ga-click$="Signed in as"] strong');
     const username = ghUsernameTag.text();
-    $('<a>', {
+    _$('<a>', {
         role: 'menuitem',
         class: 'dropdown-item',
         href: `/${username}?tab=followers`,
         text: 'Your followers',
         'data-ga-click': 'Header, go to followers, text:your followers'
     }).insertBefore(ghYourGistTag);
-    $('<a>', {
+    _$('<a>', {
         role: 'menuitem',
         class: 'dropdown-item',
         href: `/${username}?tab=followings`,
         text: 'Your followings',
         'data-ga-click': 'Header, go to followings, text:your followings'
     }).insertBefore(ghYourGistTag);
-    $('<a>', {
+    _$('<a>', {
         role: 'menuitem',
         class: 'dropdown-item',
         href: '/',
--- readme.md
+++ readme.md
@@ -7,3 +7,5 @@
 
 + [Tampermonkey中文文档（部分）](https://blog.csdn.net/abc45628/article/details/53919135)
 + [GM_addStyle equivalent in TamperMonkey](https://stackoverflow.com/questions/23683439/gm-addstyle-equivalent-in-tampermonkey)
++ [Tampermonkey documentation](https://www.tampermonkey.net/documentation.php)
++ [tobyqin/tampermonkey_vue](https://github.com/tobyqin/tampermonkey_vue)
