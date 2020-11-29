c27c3d476a6e92d6561ee3d9b969fe5d86b030d1
AoiHosizora <a970335605@hotmail.com>
2020-11-29 17:23:33 +0800

    fix(width): use MutationObserver to add octotree-wide dynamicly

--- src/ts/ui_event.ts
+++ src/ts/ui_event.ts
@@ -170,6 +170,12 @@ function bindResize(flag: boolean) {
     });
 }
 
+function addOctotreeWide(_: object) {
+    if ($('html').hasClass('octotree-pinned') && !($('html').hasClass('octotree-wide'))) {
+        $('html').addClass('octotree-wide');
+    }
+}
+
 function _regResizeEvent() {
     const el = $('#ahid-nav');
     const hdr = $('.ui-resizable-handle');
@@ -183,6 +189,12 @@ function _regResizeEvent() {
     hdr.mouseup(event);
     el.mouseup(event);
     el.resize(() => { adjustMain(true); });
+
+    const observer = new MutationObserver(addOctotreeWide);
+    observer.observe(document.getElementsByTagName('html')[0], {
+        attributes: true,
+        attributeFilter: ['class'],
+    });
 }
 
 export function getSvgTag(type: string, rate: number = 1) {
