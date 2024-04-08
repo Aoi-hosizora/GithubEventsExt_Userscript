4f363f8e1bc27bde0d5daa24bebe9c953e0d31ef
Aoi-hosizora <a970335605@hotmail.com>
2024-04-08 23:19:10 +0800

    feat: improve adjustModalDialogLayout and fix overflow

--- src/ts/ui/github/global_ui.ts
+++ src/ts/ui/github/global_ui.ts
@@ -1,8 +1,7 @@
 import $ from 'jquery';
 import { Global } from "@src/ts/data/storage";
-import { URLType } from "@src/ts/data/model";
 import { getPathTag } from "@src/ts/ui/sidebar/svg_tag";
-import { Completer, observeAttributes, observeChildChanged, getDocumentScrollYOffset } from "@src/ts/utils/utils";
+import { Completer, observeAttributes } from "@src/ts/utils/utils";
 
 // =================
 // global ui related
@@ -39,27 +38,15 @@ function adjustHovercardZindex() {
 /**
  * Adjust modal dialog for layout.
  */
-function adjustModalDialogLayout(
-    headerClassName: string,
-    ifAddedChecker: (element?: Element) => boolean,
-    adjustOverlayLayout: (element: JQuery<HTMLElement>, opened: boolean) => void,
-): Promise<boolean> {
+function adjustModalDialogLayout(headerClassName: string): Promise<boolean> {
     const completer = new Completer<boolean>();
 
+    // 1. find dialog element
     const headerDiv = $(`div.${headerClassName}`); // example: AppHeader-globalBar-start
-    const sidePanel = headerDiv.find('deferred-side-panel');
-    if (!sidePanel.length) {
+    if (!headerDiv.length) {
         completer.complete(false);
         return completer.future(); // unreachable
     }
-
-    // const modalDialogOverlay = headerDiv.find('div.Overlay-backdrop--side');
-    // const modalDialog = headerDiv.find('modal-dialog');
-    // if (!modalDialog.length || !modalDialogOverlay.length) {
-    //     completer.complete(false);
-    //     return completer.future(); // unreachable
-    // }
-
     const fragment = headerDiv.find('include-fragment');
     const modalDialog = headerDiv.find('dialog')
     if (!fragment.length || !modalDialog.length) {
@@ -67,153 +54,45 @@ function adjustModalDialogLayout(
         return completer.future(); // unreachable
     }
 
-    // observeDialogNode(); // observe unloaded node, and than observe new node, and complete completer
-    
+    // 2. observe dialog opening, and adjust body's scrollbar
     observeAttributes(modalDialog[0], (record, el) => {
         if (record.attributeName === 'open') {
-            console.log('opened by observeUnloadedNode !!!');
             var opened = el.hasAttribute('open');
-            // adjustOverlayLayout(modalDialogOverlay, opened); // adjust margin when opened
             if (opened) {
-                $('body').css('overflow', 'visible !important');
+                // $('body').css('overflow-y', 'initial !important'); // !important no working
+                $('body').attr('style', (_, s) => {
+                    let orig = s || '';
+                    return orig + ' overflow-y: initial !important;';
+                });
             }
-            // if (!opened) {
-            //     // restore scroll offset, maybe a bug of GitHub
-            //     document.documentElement.scrollTo({ top: getDocumentScrollYOffset() });
-            // }
         }
     });
 
+    // 3. observe data loaded, if loaded, complete completer
     if (!fragment[0].hasAttribute('data-loaded')) {
         observeAttributes(fragment[0], (record, _) => {
             if (record.attributeName === 'data-loaded') {
-                console.log('data-loaded added !!!');
                 completer.complete(true);
             }
         });
     } else {
-        // observeLoadedNode(); // observe new node directly
         completer.complete(true);
     }
     return completer.future();
-
-    // >>> two helpers functions
-
-    // function observeDialogNode() {
-    //     // 1. observe the unloaded node which will be deleted, or said, updated
-    //     // adjustOverlayLayout(modalDialogOverlay, false); // adjust margin first
-    //     observeAttributes(modalDialog[0], (record, el) => {
-    //         if (record.attributeName === 'open') {
-    //             console.log('opened by observeUnloadedNode !!!');
-    //             var opened = el.hasAttribute('open');
-    //             // adjustOverlayLayout(modalDialogOverlay, opened); // adjust margin when opened
-    //             if (opened) {
-    //                 $('body').css('overflow', 'visible !important');
-    //             }
-    //             // if (!opened) {
-    //             //     // restore scroll offset, maybe a bug of GitHub
-    //             //     document.documentElement.scrollTo({ top: getDocumentScrollYOffset() });
-    //             // }
-    //         }
-    //     });
-
-    //     // 2. observe the dialog helper div for node adding, and starting new observing
-    //     // var loadObserver = observeChildChanged(modalDialog[0], (record) => {
-    //     //     if (!record.addedNodes) {
-    //     //         return;
-    //     //     }
-
-    //     //     var added = false;
-    //     //     for (var node of record.addedNodes) {
-    //     //         if (node.nodeName == 'scrollable-region') {
-    //     //             added = true; // this node is added in interactive manner
-    //     //             openObserver.disconnect();
-    //     //             loadObserver.disconnect(); // after node is added, disconnect the observer
-    //     //             break;
-    //     //         }
-    //     //         // if (ifAddedChecker(node as Element) === true) {
-    //     //         // }
-    //     //     }
-    //     //     if (!added) {
-    //     //         return;
-    //     //     }
-
-    //     //     // console.log('added, observeLoadedNode');
-    //     //     // observeLoadedNode();
-    //     //     completer.complete(true);
-    //     // });
-    // }
-
-    // function observeLoadedNode() {
-    //     // 3. observe the new node which is inserted when dialog loaded
-    //     // const modalDialogOverlay = headerDiv.find('div.Overlay-backdrop--side');
-    //     // const modalDialog = headerDiv.find('modal-dialog');
-    //     // if (!modalDialog.length || !modalDialogOverlay.length) {
-    //     //     return;
-    //     // }
-    //     const modalDialog = headerDiv.find('dialog')
-    //     if (!modalDialog.length) {
-    //         completer.complete(false);
-    //         return completer.future(); // unreachable
-    //     }
-
-    //     // adjustOverlayLayout(modalDialogOverlay, true); // adjust margin first
-    //     observeAttributes(modalDialog[0], (record, el) => {
-    //         if (record.attributeName === 'open') {
-    //             console.log('opened by observeLoadedNode !!!');
-    //             var opened = el.hasAttribute('open');
-    //             // adjustOverlayLayout(modalDialogOverlay, opened);
-    //             if (opened) {
-    //                 $('body')[0].style.overflow = 'visible !important'
-    //             }
-    //             // if (!opened) {
-    //             //     // restore scroll offset, maybe a bug of GitHub
-    //             //     document.documentElement.scrollTo({ top: getDocumentScrollYOffset() });
-    //             // }
-    //         }
-    //     });
-    // }
 }
 
 /**
  * Adjust global modal dialog for its layout.
  */
 function adjustGlobalModalDialogLayout() {
-    adjustModalDialogLayout(
-        'AppHeader-globalBar-start',
-        // (element) => element?.tagName?.toLowerCase() === 'div' && element?.classList.contains('Overlay-backdrop--side') === true,
-        (element) => element?.tagName?.toLowerCase() === 'dialog' && element?.classList.contains('Overlay--placement-left') === true,
-        (element, _) => {
-            // const showOctotree = $('html').hasClass('octotree-show');
-            // const octotree = $('nav.octotree-sidebar.octotree-github-sidebar');
-            // if (showOctotree && octotree.length) {
-            //     element.css('margin-left', `${octotree.width()}px`);
-            // } else {
-            //     element.css('margin-left', '0');
-            // }
-            // $('body').css('padding-right', '0');
-            // $('body').css('overflow', 'initial');
-        },
-    );
+    adjustModalDialogLayout('AppHeader-globalBar-start');
 }
 
 /**
  * Adjust user modal dialog for its layout.
  */
 function adjustUserModalDialogLayout(): Promise<boolean> {
-    return adjustModalDialogLayout(
-        'AppHeader-user',
-        (element) => element?.tagName?.toLowerCase() === 'user-drawer-side-panel',
-        (element, _) => {
-            if (Global.urlInfo.type !== URLType.OTHER && Global.pinned) {
-                element.css('margin-right', `${Global.width}px`);
-            } else {
-                element.css('margin-right', '0');
-            }
-            $('body').css('padding-right', '0');
-            $('body').css('overflow', 'initial');
-        },
-    );
+    return adjustModalDialogLayout('AppHeader-user');
 }
 
 /**
