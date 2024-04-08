8f21bc2122bd12117670ea87684a0ca64bdefb0c
Aoi-hosizora <a970335605@hotmail.com>
2024-04-08 23:18:42 +0800

    fix: reimplement adjustModalDialogLayout for modal dialog

--- src/ts/ui/github/global_ui.ts
+++ src/ts/ui/github/global_ui.ts
@@ -46,88 +46,133 @@ function adjustModalDialogLayout(
 ): Promise<boolean> {
     const completer = new Completer<boolean>();
 
-    const headerDiv = $(`div.${headerClassName}`);
+    const headerDiv = $(`div.${headerClassName}`); // example: AppHeader-globalBar-start
     const sidePanel = headerDiv.find('deferred-side-panel');
     if (!sidePanel.length) {
         completer.complete(false);
         return completer.future(); // unreachable
     }
 
-    const modalDialogOverlay = headerDiv.find('div.Overlay-backdrop--side');
-    const modalDialog = headerDiv.find('modal-dialog');
-    if (!modalDialog.length || !modalDialogOverlay.length) {
+    // const modalDialogOverlay = headerDiv.find('div.Overlay-backdrop--side');
+    // const modalDialog = headerDiv.find('modal-dialog');
+    // if (!modalDialog.length || !modalDialogOverlay.length) {
+    //     completer.complete(false);
+    //     return completer.future(); // unreachable
+    // }
+
+    const fragment = headerDiv.find('include-fragment');
+    const modalDialog = headerDiv.find('dialog')
+    if (!fragment.length || !modalDialog.length) {
         completer.complete(false);
         return completer.future(); // unreachable
     }
 
-    if (headerDiv.find('include-fragment').length) {
-        observeTempNode(); // observe temp node, and than observe new node
+    // observeDialogNode(); // observe unloaded node, and than observe new node, and complete completer
+    
+    observeAttributes(modalDialog[0], (record, el) => {
+        if (record.attributeName === 'open') {
+            console.log('opened by observeUnloadedNode !!!');
+            var opened = el.hasAttribute('open');
+            // adjustOverlayLayout(modalDialogOverlay, opened); // adjust margin when opened
+            if (opened) {
+                $('body').css('overflow', 'visible !important');
+            }
+            // if (!opened) {
+            //     // restore scroll offset, maybe a bug of GitHub
+            //     document.documentElement.scrollTo({ top: getDocumentScrollYOffset() });
+            // }
+        }
+    });
+
+    if (!fragment[0].hasAttribute('data-loaded')) {
+        observeAttributes(fragment[0], (record, _) => {
+            if (record.attributeName === 'data-loaded') {
+                console.log('data-loaded added !!!');
+                completer.complete(true);
+            }
+        });
     } else {
-        observeRealNode(); // observe new node directly
+        // observeLoadedNode(); // observe new node directly
         completer.complete(true);
     }
     return completer.future();
 
     // >>> two helpers functions
 
-    function observeTempNode() {
-        // 1. observe the node which will be deleted
-        adjustOverlayLayout(modalDialogOverlay, false); // adjust margin first
-        var tempObserver = observeAttributes(modalDialog[0], (record, el) => {
-            if (record.attributeName === 'open') {
-                var opened = el.hasAttribute('open');
-                adjustOverlayLayout(modalDialogOverlay, opened);
-                if (!opened) {
-                    // restore scroll offset, maybe a bug of GitHub
-                    document.documentElement.scrollTo({ top: getDocumentScrollYOffset() });
-                }
-            }
-        });
+    // function observeDialogNode() {
+    //     // 1. observe the unloaded node which will be deleted, or said, updated
+    //     // adjustOverlayLayout(modalDialogOverlay, false); // adjust margin first
+    //     observeAttributes(modalDialog[0], (record, el) => {
+    //         if (record.attributeName === 'open') {
+    //             console.log('opened by observeUnloadedNode !!!');
+    //             var opened = el.hasAttribute('open');
+    //             // adjustOverlayLayout(modalDialogOverlay, opened); // adjust margin when opened
+    //             if (opened) {
+    //                 $('body').css('overflow', 'visible !important');
+    //             }
+    //             // if (!opened) {
+    //             //     // restore scroll offset, maybe a bug of GitHub
+    //             //     document.documentElement.scrollTo({ top: getDocumentScrollYOffset() });
+    //             // }
+    //         }
+    //     });
 
-        // 2. observe the header div for node adding, and starting new observing
-        var observer = observeChildChanged(sidePanel[0], (record) => {
-            if (!record.addedNodes) {
-                return;
-            }
+    //     // 2. observe the dialog helper div for node adding, and starting new observing
+    //     // var loadObserver = observeChildChanged(modalDialog[0], (record) => {
+    //     //     if (!record.addedNodes) {
+    //     //         return;
+    //     //     }
 
-            var added = false;
-            for (var node of record.addedNodes) {
-                if (ifAddedChecker(node as Element) === true) {
-                    added = true; // this node is added in interactive manner
-                    tempObserver.disconnect();
-                    observer.disconnect(); // after node is added, disconnect the observer
-                    break;
-                }
-            }
-            if (!added) {
-                return;
-            }
+    //     //     var added = false;
+    //     //     for (var node of record.addedNodes) {
+    //     //         if (node.nodeName == 'scrollable-region') {
+    //     //             added = true; // this node is added in interactive manner
+    //     //             openObserver.disconnect();
+    //     //             loadObserver.disconnect(); // after node is added, disconnect the observer
+    //     //             break;
+    //     //         }
+    //     //         // if (ifAddedChecker(node as Element) === true) {
+    //     //         // }
+    //     //     }
+    //     //     if (!added) {
+    //     //         return;
+    //     //     }
 
-            observeRealNode();
-            completer.complete(true);
-        });
-    }
+    //     //     // console.log('added, observeLoadedNode');
+    //     //     // observeLoadedNode();
+    //     //     completer.complete(true);
+    //     // });
+    // }
 
-    function observeRealNode() {
-        // 3. observe the new node which is inserted when dialog loaded
-        const modalDialogOverlay = headerDiv.find('div.Overlay-backdrop--side');
-        const modalDialog = headerDiv.find('modal-dialog');
-        if (!modalDialog.length || !modalDialogOverlay.length) {
-            return;
-        }
+    // function observeLoadedNode() {
+    //     // 3. observe the new node which is inserted when dialog loaded
+    //     // const modalDialogOverlay = headerDiv.find('div.Overlay-backdrop--side');
+    //     // const modalDialog = headerDiv.find('modal-dialog');
+    //     // if (!modalDialog.length || !modalDialogOverlay.length) {
+    //     //     return;
+    //     // }
+    //     const modalDialog = headerDiv.find('dialog')
+    //     if (!modalDialog.length) {
+    //         completer.complete(false);
+    //         return completer.future(); // unreachable
+    //     }
 
-        adjustOverlayLayout(modalDialogOverlay, true); // adjust margin first
-        observeAttributes(modalDialog[0], (record, el) => {
-            if (record.attributeName === 'open') {
-                var opened = el.hasAttribute('open');
-                adjustOverlayLayout(modalDialogOverlay, opened);
-                if (!opened) {
-                    // restore scroll offset, maybe a bug of GitHub
-                    document.documentElement.scrollTo({ top: getDocumentScrollYOffset() });
-                }
-            }
-        });
-    }
+    //     // adjustOverlayLayout(modalDialogOverlay, true); // adjust margin first
+    //     observeAttributes(modalDialog[0], (record, el) => {
+    //         if (record.attributeName === 'open') {
+    //             console.log('opened by observeLoadedNode !!!');
+    //             var opened = el.hasAttribute('open');
+    //             // adjustOverlayLayout(modalDialogOverlay, opened);
+    //             if (opened) {
+    //                 $('body')[0].style.overflow = 'visible !important'
+    //             }
+    //             // if (!opened) {
+    //             //     // restore scroll offset, maybe a bug of GitHub
+    //             //     document.documentElement.scrollTo({ top: getDocumentScrollYOffset() });
+    //             // }
+    //         }
+    //     });
+    // }
 }
 
 /**
@@ -136,17 +181,18 @@ function adjustModalDialogLayout(
 function adjustGlobalModalDialogLayout() {
     adjustModalDialogLayout(
         'AppHeader-globalBar-start',
-        (element) => element?.tagName?.toLowerCase() === 'div' && element?.classList.contains('Overlay-backdrop--side') === true,
+        // (element) => element?.tagName?.toLowerCase() === 'div' && element?.classList.contains('Overlay-backdrop--side') === true,
+        (element) => element?.tagName?.toLowerCase() === 'dialog' && element?.classList.contains('Overlay--placement-left') === true,
         (element, _) => {
-            const showOctotree = $('html').hasClass('octotree-show');
-            const octotree = $('nav.octotree-sidebar.octotree-github-sidebar');
-            if (showOctotree && octotree.length) {
-                element.css('margin-left', `${octotree.width()}px`);
-            } else {
-                element.css('margin-left', '0');
-            }
-            $('body').css('padding-right', '0');
-            $('body').css('overflow', 'initial');
+            // const showOctotree = $('html').hasClass('octotree-show');
+            // const octotree = $('nav.octotree-sidebar.octotree-github-sidebar');
+            // if (showOctotree && octotree.length) {
+            //     element.css('margin-left', `${octotree.width()}px`);
+            // } else {
+            //     element.css('margin-left', '0');
+            // }
+            // $('body').css('padding-right', '0');
+            // $('body').css('overflow', 'initial');
         },
     );
 }
@@ -174,7 +220,7 @@ function adjustUserModalDialogLayout(): Promise<boolean> {
  * Add follow* menu items to avatar dropdown menu.
  */
 function showFollowAvatarMenuItem() {
-    var modalDialog = $('div.AppHeader-user modal-dialog');
+    var modalDialog = $('div.AppHeader-user dialog');
     var avatarMenuUl = modalDialog.find('nav[aria-label="User navigation"] ul');
     if (!avatarMenuUl.length) {
         return;
